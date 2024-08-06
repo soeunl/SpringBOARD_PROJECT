1e9ab1bd30558531376def83b2d24059ea5837c8
soeunl <soeun318@gmail.com>
2024-08-06 19:52:15 +0900

    파일 공통 모듈 +Api 수업

--- src/main/java/org/choongang/tour/controllers/TourPlaceSearch.java
+++ src/main/java/org/choongang/tour/controllers/TourPlaceSearch.java
@@ -3,8 +3,8 @@ package org.choongang.tour.controllers;
 import lombok.Data;
 
 @Data
-public class TourPlaceSearch {
-    private double latitude;
-    private double longitude;
-    private int radius = 20;
+public class TourPlaceSearch { // 입력한 좌표의 반경 1km의 정보를 출력하겠다.
+    private Double latitude;
+    private Double longitude;
+    private Integer radius = 1000;
 }
--- src/main/java/org/choongang/tour/services/TourPlaceInfoService.java
+++ src/main/java/org/choongang/tour/services/TourPlaceInfoService.java
@@ -1,21 +1,55 @@
 package org.choongang.tour.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.global.rests.gov.api.ApiItem;
+import org.choongang.global.rests.gov.api.ApiResult;
 import org.choongang.tour.controllers.TourPlaceSearch;
+import org.choongang.tour.entities.QTourPlace;
 import org.choongang.tour.entities.TourPlace;
+import org.choongang.tour.repositories.TourPlaceRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
+
+import static org.springframework.data.domain.Sort.Order.asc;
 
 @Service
 @RequiredArgsConstructor
 public class TourPlaceInfoService {
 
     private final RestTemplate restTemplate;
+    private final TourPlaceRepository repository;
+
+    private String serviceKey = "CHrWrFoNSLs09ec0iaNNKpj3VuYnP%2BJA5WAtHyPqdDVCdF%2Fn1NB46%2Bfd2NDjlTiNm%2Fw48BE9guQbOo12k2a6wA%3D%3D";
 
     public List<TourPlace> getList(TourPlaceSearch search) {
+        double lat = search.getLatitude();
+        double lon = search.getLongitude();
+        int radius = search.getRadius();
 
+        String url = String.format("https://apis.data.go.kr/B551011/KorService1/locationBasedList1?MobileOS=AND&MobileApp=test&mapX=%f&mapY=%f&radius=%d&numOfRows=5000&serviceKey=%s&_type=json", lon, lat, radius, serviceKey);
+
+        try {
+            ResponseEntity<ApiResult> response = restTemplate.getForEntity(URI.create(url), ApiResult.class);
+
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody().getResponse().getHeader().getResultCode().equals("0000")) { // 여기서 가지고 온 데이터가 성공 데이터이면 또는 응답코드가 0000면
+
+                List<Long> ids = response.getBody().getResponse().getBody().getItems().getItem().stream().map(ApiItem::getContentid).toList(); // contentId 항목으로 가지고 와서
+                if (!ids.isEmpty()) {
+                    QTourPlace tourPlace = QTourPlace.tourPlace;
+                    List<TourPlace> items = (List<TourPlace>)repository.findAll(tourPlace.contentId.in(ids), Sort.by(asc("contentId"))); // 정렬 추가
+
+                    return items; // 값을 내보낸다.
+                } // endif
+            } // endif
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
